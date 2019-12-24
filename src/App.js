f5327b9e8cbf420f3ef0a2acd1e684a86775f29b
Timothy <wiliusat@oregonstate.edu>
2019-12-23 19:42:57 -0800

    mapping Recipes and pulling todos api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
+import Recipe from './Recipe';
+
+
+const App = () => {
+
+    //const APP_ID = "ac7a27fb";
+    //const APP_KEY = "160b6fb154cac60808fad8b518a415e1";
+    
+
+    //example of useState, heading increments on click (5) represents the starting number
+    const [counter,setCounter] = useState(5);
+
+    const [recipes,setRecipes] = useState([{},{},{}]);
+
+    useEffect(() => {
+      getRecipes();
+    },[]);
+
+    const getRecipes = async () => {
+      // const response = await fetch('/api/endpoints');
+      // const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+
+      const data = await response.json();
+      console.log(data);
+    
+      
+
+      // setRecipes({},{},{}); 
+      // fills the empty array with array from fetch
+      // hits is an array of recipes in data 
+      
+
+
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button 
+        className="search-button" 
+        type="submit">
+          go
+        </button>
+      </form>
+
+      <h1 onClick={()=> setCounter(counter-1)}> decreasing counter: {counter} </h1>
+
+      {recipes.map(recipe=>(
+        <Recipe/>
+      ))}
+
     </div>
   );
 }
