09df8f6a3af5ef5ea16a384c7ea6f88a4b04381e
Timothy <wiliusat@oregonstate.edu>
2019-12-23 21:57:09 -0800

    successfully passing in the bollean value from api into string into html

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
+const Todo = ({id,userId,title,completed}) => {
 
-const Todo = () => {
+    var mybool = "";
+
+    if(completed == false){
+        mybool = "not done";
+    }
+    else{
+        mybool = "done";
+    }
+
     return(
         <div>
-            <h1>id</h1>
-            <p>userId</p>
-            <p>title</p>
-            <p>status</p>
+            <h1>{id}</h1>
+            <p>{userId}</p>
+            <p>{title}</p>
+            <p>status: {mybool}</p>
         </div>
     );
 }
