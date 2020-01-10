385c59e4520760af0457728e9c543b9fbd6949ab
Timothy <wiliusat@oregonstate.edu>
2020-01-10 04:25:31 -0800

    components shell connected

---
+++ src/components/create-plan.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreatePlan extends Component{
+    render(){
+        return(
+            <div>
+                <p>You are on the Create Plan Component</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/edit-plan.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditPlan extends Component{
+    render(){
+        return(
+            <div>
+                <p>You are on the Edit Plan Component</p>
+            </div>
+        )
+    }
+}
